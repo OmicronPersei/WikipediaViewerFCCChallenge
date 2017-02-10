7d573f8018bde0451c46a28e0c8c8d02ffeabbd5
OmicronPersei <wheeler.weise@gmail.com>
2017-02-09 19:14:24 -0600

    Initial commit of search modal.

--- Javascript.js
+++ Javascript.js
@@ -1,2 +1,8 @@
 /*global $, jQuery, navigator*/
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, white: true  */
+
+$(document).ready(function() {
+  "use strict";
+  
+  $("#searchModal").modal("show");
+});
