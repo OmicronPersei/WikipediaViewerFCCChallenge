5a0596751f28c5c3e7cabb402d9eb47aee54c85f
OmicronPersei <wheeler.weise@gmail.com>
2017-02-09 19:43:35 -0600

    Added some methods for the different display types.  To implement later.

--- Javascript.js
+++ Javascript.js
@@ -1,8 +1,45 @@
 /*global $, jQuery, navigator*/
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, white: true  */
 
+var searchModal = {
+  
+  display: function(searchCallback) {
+    "use strict";
+    
+    $("#searchModal").modal("show");
+  },
+  
+  clear: function() {
+    "use strict";
+    
+  }
+};
+
+var searchResultsDisplay = {
+  
+  display: function() {
+    "use strict";
+  },
+  
+  clear: function() {
+    "use strict";
+    
+  }
+};
+
+var DisplayTypes = {
+  SearchModal: searchModal,
+  SearchResults: searchResultsDisplay
+};
+
+var changeLayout = function(DisplayType) {
+  "use strict";
+  
+  
+};
+
 $(document).ready(function() {
   "use strict";
   
-  $("#searchModal").modal("show");
+  
 });
